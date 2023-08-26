e641acb4943e272259551318178ffb46f9f32fde
Matheus Lima <teulima20@hotmail.com>
2023-08-26 18:26:24 -0300

    feat: adding images to the project

--- App.js
+++ App.js
@@ -1,18 +1,25 @@
-import 'react-native-gesture-handler';
-
 import { StatusBar } from 'expo-status-bar';
-import { AppLoading } from 'expo'
-import { useFonts, Anton_400Regular } from '@expo-google-fonts/anton';
+import React from 'react';
+// import { AppLoading } from 'expo';
+import { useFonts, Anton_400Regular} from '@expo-google-fonts/anton';
 
 import Routes from './src/router';
 
 export default function App() {
 
- 
+  const fonts = useFonts({
+    Anton_400Regular,
+  });
+
+
+  // if(!fontsLoaded){
+  //   return <AppLoading />;
+  // }
+
 
   return (
     <>
-      <StatusBar style="auto"/>
+      <StatusBar style="auto" />
       <Routes/>
     </>
   );
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,10 +1,35 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, StyleSheet, SafeAreaView, Image } from 'react-native';
+
+import BannerFoto from '../../assets/banner.png';
 
 export default function Home() {
  return (
-   <View>
-      <Text style={{marginTop: 60, fontSize: 20}}>Página Home</Text>
-   </View>
+   <SafeAreaView style={styles.container}>
+     <View style={styles.header}>
+       <Image
+         source={BannerFoto}
+         style={styles.image}
+       />
+
+       <View>
+          
+       </View>
+     </View>
+   </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    width: "100%",
+    backgroundColor: "#FFF"
+  },
+  header: {
+    marginBottom: 8
+  },
+  image: {
+    width: "#100%"
+  }
+});
